81b8e928fa0274c8d8092b163ceb4a4e92f5b0eb
kfuentes466 <36283211+kfuentes466@users.noreply.github.com>
2020-10-03 04:45:22 -0600

    Ya ingresa y muestra, falta buscar y hacer que cambie según vista

--- Practico2/src/app/app.module.ts
+++ Practico2/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,26 +13,43 @@ import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuth, AngularFireAuthModule } from "@angular/fire/auth";
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 import { UsuarioService } from './servicios/usuario.service';
 import { VrificarCorreoComponent } from './componentes/vrificar-correo/vrificar-correo.component';
+import { HistorialComponent } from './componentes/historial/historial.component';
+import { HistorialListaComponent } from './componentes/historial-lista/historial-lista.component';
+import { ClienteService } from './servicios/clientes/cliente.service';
+//Toastr
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [
     AppComponent,
     PantallaPrincipalComponent,
     IniciaSesionComponent,
     ResgistrateComponent,
-    VrificarCorreoComponent
+    VrificarCorreoComponent,
+    HistorialComponent,
+    HistorialListaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
     //AngularFireAuth,
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    //Toastr
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    UsuarioService,
+    ClienteService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
